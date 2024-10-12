88046fa9dbb6ca4c8ba3f81292995a6433448388
kenny <kenny909@mat.umk.pl>
2024-10-12 18:04:04 +0200

    namespace url name

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+app_name = "events"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:event_id>/", views.event_detail, name="detailed_event"),
