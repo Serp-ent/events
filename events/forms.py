2e29b070a23de97125e7ef1622aa48f90808ff74
kenny <kenny909@mat.umk.pl>
2024-10-27 16:27:31 +0100

    Make photo non-required field

--- events/forms.py
+++ events/forms.py
@@ -5,4 +5,6 @@ from django import forms
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ["name", "description", "startAt", 'photo']
+        fields = ["name", "description", "startAt", "photo"]
+
+    photo = forms.ImageField(required=False)
