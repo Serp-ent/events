51914ee400ce342526558f17c5138e17fadc7244
kenny <kenny909@mat.umk.pl>
2024-10-28 23:13:51 +0100

    Add tailwind support

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './events/templates/**/*.html',
+    './events/static/**/*.js', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
