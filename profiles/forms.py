e0d8e9acfd85e6a3368ae700a0e479841523fc3a
kenny <kenny909@mat.umk.pl>
2024-11-01 00:57:15 +0100

    Add profile editing

---
+++ profiles/forms.py
@@ -0,0 +1,25 @@
+from .models import Profile
+from django import forms
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["bio", "location", "birth_date"]
+        widgets = {
+            "bio": forms.Textarea(
+                attrs={
+                    "class": "w-full p-2 border border-gray-300 rounded resize-none",
+                    "rows": 5,
+                }
+            ),
+            "location": forms.TextInput(
+                attrs={"class": "w-full p-2 border border-gray-300 rounded"}
+            ),
+            "birth_date": forms.DateInput(
+                attrs={
+                    "class": "w-full p-2 border border-gray-300 rounded",
+                    "type": "date",
+                }
+            ),
+        }
--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Profile
+from .forms import ProfileForm
 from django.http import HttpResponse, HttpRequest
 
 # TODO: login required
+
 
 def profile_detail(request: HttpRequest, user_id: int) -> HttpResponse:
     profile: Profile = get_object_or_404(Profile, user_id=user_id)
     return render(request, "profiles/profile_detail.html", {"profile": profile})
 
 
+# TODO: login required
 def profile_edit(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Edit profile")
+    profile = request.user.profile
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("profiles:profile_detail", user_id=request.user.id)
+    else:
+        form = ProfileForm(instance=profile)
+
+    context = {"form": form}
+    return render(request, "profiles/edit_profile.html", context)
